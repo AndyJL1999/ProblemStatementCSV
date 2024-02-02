9456a53c9d2cbbb725d9b1b44616a72291d37637
AndyJL1999 <andylainez115@hotmail.com>
2024-02-02 16:23:41 -0500

    Add project files.

---
+++ ProblemStatementCSV/Problem.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace ProblemStatementCSV
+{
+    partial class Problem
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button = new System.Windows.Forms.Button();
+            this.textField = new System.Windows.Forms.TextBox();
+            this.statusText = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button
+            // 
+            this.button.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button.Location = new System.Drawing.Point(440, 206);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(100, 35);
+            this.button.TabIndex = 0;
+            this.button.Text = "Click Me";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.button_Click);
+            // 
+            // textField
+            // 
+            this.textField.BackColor = System.Drawing.Color.White;
+            this.textField.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textField.Location = new System.Drawing.Point(234, 206);
+            this.textField.Multiline = true;
+            this.textField.Name = "textField";
+            this.textField.ReadOnly = true;
+            this.textField.Size = new System.Drawing.Size(200, 35);
+            this.textField.TabIndex = 1;
+            this.textField.WordWrap = false;
+            // 
+            // statusText
+            // 
+            this.statusText.AutoSize = true;
+            this.statusText.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.statusText.Location = new System.Drawing.Point(250, 150);
+            this.statusText.Name = "statusText";
+            this.statusText.Size = new System.Drawing.Size(0, 36);
+            this.statusText.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(234, 248);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(296, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Mission: Get last 10 digits from the sum of all rows";
+            // 
+            // Problem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.statusText);
+            this.Controls.Add(this.textField);
+            this.Controls.Add(this.button);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Problem";
+            this.Text = "Problem";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button;
+        private System.Windows.Forms.TextBox textField;
+        private System.Windows.Forms.Label statusText;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ProblemStatementCSV/Problem.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Drawing;
+using System.IO;
+using System.Numerics;
+using System.Windows.Forms;
+
+namespace ProblemStatementCSV
+{
+    public partial class Problem : Form
+    {
+        public Problem()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            string csv = GetCSVFromFileExplorer();
+
+            // Check if a csv file was returned
+            if(csv != null)
+            {
+                ReadCSV(csv);
+
+                statusText.ForeColor = Color.LimeGreen;
+                statusText.Text = "Mission Accomplished!";
+            }
+            else
+            {
+                statusText.ForeColor = Color.Red;
+                statusText.Text = "No CVS file in sight.";
+            }
+        }
+
+        private void ReadCSV(string csvFilePath)
+        {
+            BigInteger total = 0;
+
+            using (TextFieldParser parser = new TextFieldParser(csvFilePath))
+            {
+                // Set the delimiter of your csv file
+                parser.SetDelimiters("'");
+
+                while (parser.EndOfData == false)
+                {
+                    string[] fields = parser.ReadFields();
+
+                    BigInteger.TryParse(fields[1], out BigInteger value);
+                    total += value;
+                }
+            }
+            
+            // Display last 10 digits of sum and get absolute value to combat negtive values
+            textField.Text = BigInteger.Abs(total % 10000000000).ToString();
+        }
+
+
+        private string GetCSVFromFileExplorer()
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+
+            dialog.Filter = "CSV files (*.csv)|*.csv";
+            dialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+            // Only return CSV files or else... null
+            if (dialog.ShowDialog() == DialogResult.OK && Path.GetExtension(dialog.FileName) == ".csv")
+            {
+                return dialog.FileName;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
